f63a8aace9ea82cfddcff8d441a48687594e3a96
Alice_Silva <159441822+AliceKaleno@users.noreply.github.com>
2026-05-12 18:25:20 -0300

    Update database.js

--- database.js
+++ database.js
@@ -1,1 +1,31 @@
+import sqlite3 from "sqlite3";
+import { open } from "sqlite";
 
+export async function conectarDB() {
+
+  const db = await open({
+    filename: "./adminflow.db",
+    driver: sqlite3.Database,
+  });
+
+  await db.exec(`
+    CREATE TABLE IF NOT EXISTS usuarios (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      nome TEXT,
+      email TEXT UNIQUE,
+      senha TEXT,
+      role TEXT
+    )
+  `);
+
+  await db.exec(`
+    CREATE TABLE IF NOT EXISTS vendas (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      cliente TEXT,
+      valor REAL,
+      status TEXT
+    )
+  `);
+
+  return db;
+}
